8521d73da8882b6dfb6d5017ce7f1b35ad9bf86d
knostijr <christophkonst@icloud.com>
2026-01-15 14:41:10 +0100

    auth_app serializer, create user, password validation

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Serializer für User-Registrierung
+    Validiert Passwort und erstellt neuen User
+    """
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password_confirm = serializers.CharField(
+        write_only=True,
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'password_confirm']
+        read_only_fields = ['id']
+
+    def validate(self, attrs):
+        """
+        Validiere, dass beide Passwörter übereinstimmen
+        """
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({
+                "password": "Password fields didn't match."
+            })
+        return attrs
+
+    def create(self, validated_data):
+        """
+        Erstelle User mit gehashtem Passwort
+        """
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer für User-Darstellung (ohne Passwort)
+    """
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+        read_only_fields = ['id']
